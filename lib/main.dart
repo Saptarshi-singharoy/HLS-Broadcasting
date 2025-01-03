a20688d9dd53dc8a45e5083387c180f02026a629
Saptarshi Singha Roy <135221302+Saptarshi-singharoy@users.noreply.github.com>
2025-01-03 11:54:54 +0530

    playback url link input

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
         children: [
           Platform.isAndroid
-              ? const AndroidPlayer(
-                  videoUrl:
-                      "https://eab30f14a9fe.ap-south-1.playback.live-video.net/api/video/v1/ap-south-1.730335194207.channel.Rqyu1GDAcfsD.m3u8")
+              ? IntroScreen()
               : TextButton(
                   onPressed: () async {
                     try {
@@ -49,6 +47,59 @@ class AndroidPlayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(child: MyVideoView(videoUrl: videoUrl));
+  }
+}
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+            controller: _controller,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      _controller.clear();
+                    },
+                    icon: Icon(Icons.clear))),
+          ),
+          MaterialButton(
+            color: Colors.amber[600],
+            onPressed: () {
+              if (_controller.text != null || _controller.text.isNotEmpty) {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (context) => AndroidPlayer(
+                            videoUrl: _controller.text,
+                          )),
+                );
+              }
+            },
+            child: Text('Watch'),
+          )
+        ],
+      ),
+    );
   }
 }
 
